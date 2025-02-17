d096e1dbb59c656fc6dec4aed7c5d0ec54700ed2
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2025-02-17 09:49:10 +0300

    add last marks

--- src/datamanager.cpp
+++ src/datamanager.cpp
@@ -2,4 +2,20 @@
 
 DataManager::DataManager(QObject *parent)
     : QObject{parent}
-{}
+{
+
+}
+
+void DataManager::refreshInterfaces()
+{
+    m_interfaceNameModel.clear();
+
+    auto interfaces = QNetworkInterface::allInterfaces();
+    for (auto &it: interfaces) {
+        m_interfaceNameModel.append( it.name() );
+        qDebug() << it.name();
+      //  readInterfaceConfig( it.name() );
+        emit interfaceNameModelChanged();
+    }
+
+}
--- src/datamanager.h
+++ src/datamanager.h
@@ -1,13 +1,24 @@
 #pragma once
 
 #include <QObject>
+#include <QDebug>
+#include <QNetworkInterface>
+
+#include "devicemodel.h"
 
 class DataManager : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QStringList interfaceNameModel MEMBER m_interfaceNameModel NOTIFY interfaceNameModelChanged)
 public:
     explicit DataManager(QObject *parent = nullptr);
+    Q_INVOKABLE void refreshInterfaces();
+signals:  
+    void interfaceNameModelChanged();
 
-signals:
+private:
+    DeviceModel m_deviceModel;
+
+    QStringList m_interfaceNameModel;
 };
 
