7c2ed6bf1c23c78c4d072ac3b9ac2fea0b5e9fcd
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2025-02-21 08:37:00 +0300

    add init struct fields

--- src/ethdevice.h
+++ src/ethdevice.h
@@ -1,39 +1,40 @@
 #pragma once
 
-#include  <QString>
+#include <QString>
+#include <QObject>
 
 struct EthDevice {
     // 26 fields
-    QString m_interfaceName{};
-    QString m_sysDevPath{};
+    QString m_interfaceName{QObject::tr("Unknown")};
+    QString m_sysDevPath{QObject::tr("Unknown")};
     bool m_carrier{false};
     //IP
-    int m_MTU{};
-    QString m_MAC;
-    QString m_IP;
-    QString m_netmask;
+    int m_MTU{0};
+    QString m_MAC{QObject::tr("N/A")};
+    QString m_IP{ QObject::tr("Not Connected")};
+    QString m_netmask{ QObject::tr("Not Connected") };
     // general
-    int m_updateInterval{};
+    int m_updateInterval{1000};
     bool m_monitoring{false};
     bool m_notifications{false};
     // icon view
-    int m_theme{};
+    int m_theme{0};
     // chart view
-    uint m_chartUplColor{}; // save and restore QColor.rgba() and QColor::fromRgba(value.toUInt());
-    uint m_chartDldColor{};
-    uint m_chartBgColor{};
+    uint m_chartUplColor{0}; // save and restore QColor.rgba() and QColor::fromRgba(value.toUInt());
+    uint m_chartDldColor{0};
+    uint m_chartBgColor{0};
     bool m_chartTransparentBackground{false};
     //	Rx and Tx to bytes and packets
-    quint64 m_maxSpeed{};
-    quint64 m_byteSpeedRx{};
-    quint64 m_byteSpeedTx{};
-    quint64 m_pktSpeedRx{};
-    quint64 m_pktSpeedTx{};
+    quint64 m_maxSpeed{0};
+    quint64 m_byteSpeedRx{0};
+    quint64 m_byteSpeedTx{0};
+    quint64 m_pktSpeedRx{0};
+    quint64 m_pktSpeedTx{0};
 
     // Statistics
-    quint64 m_totalBytesRx{};
-    quint64 m_totalBytesTx{};
-    quint64 m_totalPktRx{};
-    quint64 m_totalPktTx{};
+    quint64 m_totalBytesRx{0};
+    quint64 m_totalBytesTx{0};
+    quint64 m_totalPktRx{0};
+    quint64 m_totalPktTx{0};
     int m_busystate{0};
 };
